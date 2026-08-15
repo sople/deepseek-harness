339bf2a05698397611d2df4a105f034aba69eff8
Cody.Liu <22427922+sople@users.noreply.github.com>
2026-08-15 21:14:37 +0800

    test plugin

---
+++ sople/src/hello.ts
@@ -0,0 +1,8 @@
+import type { Context } from '@deepseek-ai/cordis'
+
+export const name = 'hello-plugin'
+
+export function apply(ctx: Context) {
+  // Required dependencies are ready before apply runs.
+  console.log('[hello-plugin] plugin loaded!')
+}
